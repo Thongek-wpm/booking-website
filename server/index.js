70529ac1b164bbeb20741700b76e5614c83b1d36
Thongek Wogponom <thongek.wpm@gmail.com>
2023-09-14 09:44:58 +0700

    commit updata

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import Express from "express";
+import mongoose from "mongoose";
+
+const app = Express();
+dotenv.config();
+
+//เชื่อมกับmongoDB
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mongoDB,For you can using mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+//แจ้งสถานะของตัวMongoDB
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB Disconnected!");
+});
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB Connected!");
+});
+
+app.get("/api", (req, res) => {
+  res.send("Hey, First requset!");
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to Back-End,Now you can developing.");
+});
+//mongodb+srv://thongekwpm:A6UEYrkxzOlxsuh1@cluster0.wiims9k.mongodb.net/?retryWrites=true&w=majority
