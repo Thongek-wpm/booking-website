762f31fc2187fd5039d70daa8e6164931a18f079
Thongek Wogponom <thongek.wpm@gmail.com>
2023-09-14 10:15:50 +0700

    commit updata

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,4 +2,12 @@ import Express from "express";
 
 const router = Express.Router();
 
+router.get("/", (req, res) => {
+  res.send("Hello, this's auth end-point");
+});
+
+router.get("/register", (req, res) => {
+  res.send("Hello, this's register end-point");
+});
+
 export default router;
--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -2,12 +2,6 @@ import Express from "express";
 
 const router = Express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello, this's auth end-point");
-});
 
-router.get("/register", (req, res) => {
-  res.send("Hello, this's register end-point");
-});
 
 export default router;
