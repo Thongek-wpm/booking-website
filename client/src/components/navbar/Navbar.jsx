5df80ca5f36da2bdf1cf03e77fb96b14be1cb769
Thongek Wogponom <thongek.wpm@gmail.com>
2023-09-15 13:14:38 +0700

    Update Navbar.jsx

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,17 +1,17 @@
-import "./navbar.css"
+import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">lamabooking</span>
+        <span className="logo">Next Warehouse</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
