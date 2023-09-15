10e2782513d8b4262f2ace5b77e45206b8feb80b
Thongek Wogponom <thongek.wpm@gmail.com>
2023-09-15 12:45:52 +0700

    coomit updata

--- server/controllers/hotel.js
+++ server/controllers/hotel.js
@@ -1,59 +1,65 @@
 import Hotel from "../models/Hotel.js";
+import Room from "../models/Room.js";
 
-//CREATE
 export const createHotel = async (req, res, next) => {
   const newHotel = new Hotel(req.body);
 
   try {
-    const saveHotels = await newHotel.save();
-    res.status(200).json(saveHotels);
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
   } catch (err) {
     next(err);
   }
 };
-
-//UPDATA
 export const updataHotels = async (req, res, next) => {
   try {
-    const updataHotels = await Hotel.findByIdAndUpdate(
+    const updatedHotel = await Hotel.findByIdAndUpdate(
       req.params.id,
-      {
-        $set: req.body,
-      },
+      { $set: req.body },
       { new: true }
     );
-    res.status(200).json(updataHotels);
+    res.status(200).json(updatedHotel);
   } catch (err) {
     next(err);
   }
 };
-
-//DELETE
 export const deleteHotel = async (req, res, next) => {
   try {
     await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json("hotel has been deleted!!");
+    res.status(200).json("Hotel has been deleted.");
   } catch (err) {
     next(err);
   }
 };
-
-//GET
-
 export const getHotel = async (req, res, next) => {
   try {
     const hotel = await Hotel.findById(req.params.id);
     res.status(200).json(hotel);
   } catch (err) {
-    res.status(500).json(err);
+    next(err);
   }
 };
-
-//GET ALL
 export const getHotels = async (req, res, next) => {
+  const { min, max, ...others } = req.query;
   try {
-    const hotels = await Hotel.find();
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+    }).limit(req.query.limit);
     res.status(200).json(hotels);
+  } catch (err) {
+    next(err);
+  }
+};
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    res.status(200).json(list);
   } catch (err) {
     next(err);
   }
--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -6,6 +6,7 @@ import {
   getHotel,
   getHotels,
   updataHotels,
+  countByCity,
 } from "../controllers/hotel.js";
 import { verifyAdmin, verifyUser, verifyToken } from "../utils/verifyToken.js";
 
@@ -21,9 +22,10 @@ router.put("/:id", verifyAdmin, updataHotels);
 router.delete("/:id", verifyAdmin, deleteHotel);
 
 //GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 
 //GET ALL
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
 
 export default router;
