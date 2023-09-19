32c92c2fbf4fc3c904d4f0a7b1d67edf226bcdc8
Thongek Wogponom <thongek.wpm@gmail.com>
2023-09-19 11:26:00 +0700

    Update useFetech.js

--- client/src/hooks/useFetech.js
+++ client/src/hooks/useFetech.js
@@ -10,7 +10,7 @@ const useFetch = (url) => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const res = axios.get(url);
+        const res = await axios.get(url);
         setData(res.data);
       } catch (err) {
         setError(err);
@@ -23,7 +23,7 @@ const useFetch = (url) => {
   const reFetch = async () => {
     setLoading(true);
     try {
-      const res = axios.get(url);
+      const res = await axios.get(url);
       setData(res.data);
     } catch (err) {
       setError(err);
